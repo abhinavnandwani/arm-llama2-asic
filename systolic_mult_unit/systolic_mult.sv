8c3433479944825d25efd93759b6ed13a9952c5f
Abhinav Nandwani <abhinav@Abhinavs-MacBook-Pro.local>
2024-10-22 16:35:58 -0500

    systolic array generate block

--- systolic_mult_unit/systolic_mult.sv
+++ systolic_mult_unit/systolic_mult.sv
@@ -11,4 +11,14 @@ module systolic_mult();
     input [15:0] B_north [0:31]; //weights, kernals from weight buffer/memory 
     
 
-    pe pe_units [0:31][0:31] (.clk(clk), .rst_n(rst_n), .west_row_in())
+
+    genvar i,j;
+    generate
+        for(i = 0;i<32;i++) begin //iterate through rows
+            for(j = 0;j<32;j++) begin //iterate through cols
+
+                //pe instantiation
+                pe peDUT(.clk(clk), .rst_n(rst_n), .west_row_in((j == 0)?A_west[i]:east_row_out[i-1]), .north_col_in(), .east_row_out(), .south_col_out());
+                
+            end
+        end
